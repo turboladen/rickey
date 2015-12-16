9c0ed0f95fe6329799b9e1e30e9b99a7863928bd
Steve Loveless <steve.loveless@gmail.com>
2015-12-15 22:56:38 -0800

    Create, delete concoction-types

--- app/pods/concoction-types/route.js
+++ app/pods/concoction-types/route.js
@@ -1,7 +1,29 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-  model () {
+  model() {
     return this.store.findAll('concoction-type')
+  },
+
+  actions: {
+    addConcoctionType: function() {
+      let concoctionType = this.store.createRecord('concoction-type', {
+        name: this.controller.get('name')
+      })
+
+      concoctionType.save().then(() => {
+        console.log('save successful')
+        this.controller.set('name', null)
+        this.refresh()
+      }).catch(() => {
+        console.log('save failed')
+      })
+    },
+
+    deleteConcoctionType: function(selectedRecord) {
+      selectedRecord.destroyRecord().then(() => {
+        this.refresh()
+      })
+    }
   }
 })
